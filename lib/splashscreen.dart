f02fd557277ee69ea2ab6b4d4afd9f016983dd40
IamGiel <lfagel84@gmail.com>
2020-10-16 11:52:21 -0400

    first

---
+++ lib/splashscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_explore/home.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplash extends StatefulWidget {
+  @override
+  _MySplashState createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('This works'));
+  }
+}
